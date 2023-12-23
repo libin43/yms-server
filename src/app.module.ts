7473efab679bc91fe124a88f9c7b92c3c581a6e8
libin43 <libinbiji43@gmail.com>
2023-12-23 15:58:40 +0530

    config: graphql along with apollo server

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { YardModule } from './yard/yard.module';
+
 @Module({
-  imports: [],
+  imports: [
+    YardModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      // definitions: {
+      //   path: join( process.cwd(), 'src/graphql.schema.ts')
+      // },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
